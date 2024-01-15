6c87530dd1a240c5a08800d81413677c899fe29d
KimYongjin <dydwlsdlz0527@gmail.com>
2024-01-15 22:59:04 +0900

    xml을 사용한 AppConfig 설정

--- Back-End/core/src/main/java/hello/core/OrderApp.java
+++ Back-End/core/src/main/java/hello/core/OrderApp.java
@@ -13,7 +13,6 @@ public class OrderApp {
 
     public static void main(String[] args) {
 
-        
         /**
          *    MemberService memberService = new MemberServiceImpl();
          *    OrderService orderService = new OrderServiceImpl();
