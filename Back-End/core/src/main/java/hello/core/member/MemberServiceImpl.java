4a4b3d23c82a385287d650f216947a2bec4fbace
KimYongjin <dydwlsdlz0527@gmail.com>
2024-01-16 23:27:52 +0900

    싱글톤 확인 테스트 완료

--- Back-End/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Back-End/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,13 +3,15 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     /**
-     *  AppConfig 사용 전에는 ServiceImpl에서 생성자를 만듬.
-     *  private final MemberRepository memberRepository = new MemoryMemberRepository();
+     * AppConfig 사용 전에는 ServiceImpl에서 생성자를 만듬.
+     * private final MemberRepository memberRepository = new
+     * MemoryMemberRepository();
      */
     private final MemberRepository memberRepository;
 
     /**
      * AppConfig가 생성자를 주입해준다.
+     * 
      * @param memberRepository
      */
     public MemberServiceImpl(MemberRepository memberRepository) {
@@ -24,6 +26,11 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 
 }
--- Back-End/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Back-End/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,23 +4,23 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
     /**
      * AppConfig 사용 전에는 ServiceImpl에서 생성자를 만듬.
      * private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
      * private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-     * private final MemberRepository memberRepository = new MemoryMemberRepository();
+     * private final MemberRepository memberRepository = new
+     * MemoryMemberRepository();
      */
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-    
+
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -29,5 +29,10 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
-    
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
 }
